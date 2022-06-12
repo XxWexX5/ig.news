3efb541c8f9cbe2cc9a93166108aed9bc7beab82
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-06-12 19:20:07 -0300

    feat(Home): Adding SSG.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import styles from "./home.module.scss";
 
@@ -40,7 +40,7 @@ export default function App({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1L9xABHY6vu00bvsuVlFpLTh");
 
   const product = {
@@ -55,5 +55,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24h
   };
 };
